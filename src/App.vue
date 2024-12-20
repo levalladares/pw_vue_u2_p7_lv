228d3ffa0f340768098ae7fc78326214a80f03b4
Luis <levalladares>
2024-12-20 00:46:06 +0000

    Taller18

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png"/>
-  <Contador />
-  <Contador />
-  <Contador />
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Contador encabezado="Contador Primero" :valor="calcular(1, 3)" :esVerdad="muestra1" :valor2="78" />
+  <Contador encabezado="Contador Segundo" v-bind:valor="calcular(5, 3)" :esVerdad="muestra2" />
+  <button @:click="cambiar(1)">Cambiar Primero</button>
+  <button @:click="cambiar(2)">Cambiar Segundo</button>
 </template>
 
 <script>
@@ -11,7 +12,26 @@ export default {
   name: 'App',
   components: {
     Contador
-  }
+  },
+  // no deberia programarse en el App
+  methods: {
+    calcular(a, b) {
+      return a + b;
+    },
+    cambiar(valor) {
+      if (valor === 1) {
+        this.muestra1 = !this.muestra1;
+      } else {
+        this.muestra2 = !this.muestra2;
+      }
+    }
+  },
+  data() {
+    return {
+      muestra1: true,
+      muestra2: true
+    }
+  },
 }
 </script>
 
--- src/components/Contador.vue
+++ src/components/Contador.vue
@@ -1,48 +1,79 @@
 <template>
-  <h2>{{ titulo }}</h2>
-  <p>{{ numero }} <sup>2</sup> ={{ calcularCuadradoComputado }} </p>
-  <p>{{ numero }} <sup>2</sup> ={{ calcularCuadradoComputado }} </p>
-  <p>{{ numero }} <sup>2</sup> ={{ calcularCuadradoComputado }} </p>
+  <h2>{{ encabezado }} : {{ valor2 }}</h2>
   <p>{{ numero }} <sup>2</sup> ={{ calcularCuadradoComputado }} </p>
   <p>{{ numero }} <sup>2</sup> ={{ calcularCuadradoComputado }} </p>
   <div>
     <button v-on:click="incrementar()">+1</button>
-    <button v-on:click="decrementar()">-1</button>
+    <button @:click="decrementar()">-1</button>
   </div>
-
+  <div v-if="esVerdad">
+    <h1>Feliz Navidad</h1>
+  </div>
+  <hr>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      numero: 10,
+      numero: this.valor,
       titulo: 'Contador'
     }
   },
   methods: {
-    calcularCuadraro(){
+    calcularCuadraro() {
       console.log('entro a calcular');
       return this.numero * this.numero;
     },
-    incrementar(){
+    incrementar() {
       this.numero++;
     },
-    decrementar(){
+    decrementar() {
       this.numero--;
     }
   },
-  computed:{
-    calcularCuadradoComputado(){
+  computed: {
+    calcularCuadradoComputado() {
       console.log('entro a calcular Computado');
       return this.numero * this.numero;
     }
+  },
+  //PRIMERA FORMADE DECLARAR PROPS
+  //props:['encabezado','valor']
+  //SEGUNDA FORMA DE DECLARAR PROPS
+  props: {
+    encabezado: String,
+    valor: Number,
+    valor2: {
+      type: Number,
+      required: false,
+      default: 77,
+      validator(value){
+        //programo mi validacion bajo mi criterio y retorno true(cuando es valido) o false
+        return value <= 77;
+      }
+    },
+    //validar que el encabezado no tenga la letra a 
+    encabezado: {
+      type: String,
+      required: true,
+      validator(value){
+        return value.indexOf('a') < 0;
+      }
+    },
+    esVerdad: {
+      type: Boolean,
+      required: true,
+    },
+    arreglo: Array,
+    fecha: Date,
+    objetoPersona: Object,
   }
 };
 </script>
 
 <style>
-button{
+button {
   background-color: #64b687;
   border-radius: 5px;
   border: 1px solid black;
@@ -50,8 +81,9 @@ button{
   cursor: pointer;
   margin: 0px 5px;
   padding: 5px 15px;
-} 
-button:hover{
+}
+
+button:hover {
   background-color: #5AA67B;
 }
 </style>
